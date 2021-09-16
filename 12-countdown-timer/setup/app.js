437ca872daf3dc701cff0e15bd387c11b6066162
nika <cv3910@innovaccion.mx>
2021-09-16 15:21:54 -0500

    fifth-commit

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,74 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+//targeting html elements
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+//**this adds 10 days to the countdown everytime you load the page**
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+//Setting the deadline  //year, month, day, hr, min, sec
+//let futureDate = new Date(2021, 9, 03, 11, 11, 11);
+//the date up is the represents a deadline for my bday :cake:
+
+//storing the date in dif variables
+//remember that the futureDate variable used to be the commented above
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 21, 30, 0);
+const year = futureDate.getFullYear();
+let month = futureDate.getMonth();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+const secs = futureDate.getSeconds();
+//day
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${months[month]} ${year} at ${hours}:${mins}am`;
+
+
+//future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  //values in ms *how many ms are in?:
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMin = 60 * 1000;
+
+  //calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMin);
+  let seconds = Math.floor((t % oneMin) / 1000);
+
+  //set values array
+  const values = [days, hours, minutes, seconds];
+  //adds zero is the value is a single digit
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+  //passes the time values to the selected html elements
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`
+  }
+}
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -1,11 +1,76 @@
 // ****** SELECT ITEMS **********
+const alertV = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
-
+let editElement;
+let editFLag = false;
+let editID = "";
 // ****** EVENT LISTENERS **********
-
+//submit form
+form.addEventListener("submit", addItem);
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  //creates an everchanging id number
+  const id = new Date().getTime().toString();
+  if (value && !editFLag) {
+    //adds new element to the list creating an html object*
+    const element = document.createElement("article");
+    //add class to the previous element
+    element.classList.add("grocery-item");
+    //add id to the previous element
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = ` <p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+    //append child to the list element in the html
+    list.appendChild(element);
+    //display alert
+    displayAlert("item added to the list", "success");
+    //show container
+    container.classList.add('show-container');
+    //add to local to local storage
+    addToLocalStorage(id, value);
+    //set back to default+
+    setBackTodDefault()
+  } else if (value && editFLag) {
+    console.log("editing");
+  } else {
+    displayAlert("please enter value", "danger");
+  }
+}
+//display alert
+function displayAlert(text, action) {
+  alertV.textContent = text;
+  alertV.classList.add(`alert-${action}`);
 
+  //remove alert
+  setTimeout(function () {
+    alertV.textContent = "";
+    alertV.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+//set back to default
+function setBackTodDefault(){
+  console.log('SET BACK TO DEFAULT')
+}
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage (id, value){
+  console.log('added to local storage');
+}
 // ****** SETUP ITEMS **********
